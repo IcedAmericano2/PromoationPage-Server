251616dff8797ccb8f36a32800f0ddf4a3c747bf
kimyubi <ieieie0419@gmail.co,=m>
2023-10-12 00:58:34 +0900

    SecurityConfig 파일 수정

--- src/main/java/com/example/promotionpage/global/config/SecurityConfig.java
+++ src/main/java/com/example/promotionpage/global/config/SecurityConfig.java
@@ -6,12 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
