6457b6bf7bb0b2d41a7cdfe6c01bb799daf295a1
kimyubi <ieieie0419@gmail.co,=m>
2023-10-11 19:51:44 +0900

    Init : 초기 개발 환경 세팅

---
+++ src/main/java/com/example/promotionpage/PromotionPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.promotionpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionPageApplication.class, args);
+	}
+
+}
