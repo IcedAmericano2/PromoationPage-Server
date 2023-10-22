3e3ab738e12b00843d0261a6ec0cf7356853f099
kimyubi <ieieie0419@gmail.co,=m>
2023-10-22 15:44:16 +0900

    Refactor : Cors 설정 리팩토링

--- src/main/java/com/example/promotionpage/global/config/CorsConfig.java
+++ src/main/java/com/example/promotionpage/global/config/CorsConfig.java
@@ -2,28 +2,23 @@ package com.example.promotionpage.global.config;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
+public class CorsConfig  implements WebMvcConfigurer {
 
-		config.setAllowCredentials(true);
-		config.setAllowedOrigins(List.of("http://localhost:3000"));
-		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-		config.setAllowedHeaders(List.of("*"));
-		config.setExposedHeaders(List.of("*"));
-
-		source.registerCorsConfiguration("/**", config);
-		return source;
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("*")
+			.allowedHeaders("*")
+			.maxAge(3600);
 	}
-
 }
